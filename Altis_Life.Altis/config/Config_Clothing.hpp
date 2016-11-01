fc3ccab570c51b653b91582289c7923a14c780ee
BlubberBerndLP <nuding69@gmail.com>
2016-11-01 21:09:11 +0100

    change

--- Altis_Life.Altis/config/Config_Clothing.hpp
+++ Altis_Life.Altis/config/Config_Clothing.hpp
@@ -3,14 +3,15 @@
 *        0: STRING (Classname)
 *        1: STRING (Display Name, leave as "" for default)
 *        2: SCALAR (Price)
-*        4: ARRAY (This is for limiting items to certain things)
-*            0: Variable to read from
-*            1: Variable Value Type (SCALAR / BOOL / EQUAL)
-*            2: What to compare to (-1 = Check Disabled)
+*        3: STRING (Conditions) - Must return boolean :
+*            String can contain any amount of conditions, aslong as the entire
+*            string returns a boolean. This allows you to check any levels, licenses etc,
+*            in any combination. For example:
+*                "call life_coplevel && license_civ_someLicense"
+*            This will also let you call any other function.
 *
 *   Clothing classnames can be found here: https://community.bistudio.com/wiki/Arma_3_CfgWeapons_Equipment
 *   Backpacks/remaining classnames can be found here (TIP: Search page for "pack"): https://community.bistudio.com/wiki/Arma_3_CfgVehicles_EMPTY
-*   DLC Clothing Items added by [TIG] Alex
 *
 */
 class Clothing {
